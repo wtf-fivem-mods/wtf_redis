46838b7ea4eab90a7edea11ab78315dab88afe45
wtf <49658883+wtf-fivem-mods@users.noreply.github.com>
2019-04-16 11:59:51 -0700

    Use metatable for redis cmds

--- lib/redis.lua
+++ lib/redis.lua
@@ -1,17 +1,20 @@
 cbs = { }
 
+Redis = {} -- see setmetatable below
+
 -- local ns = GetCurrentResourceName()
 -- local ev = string.format('wtf_redis:ns_%s', ns)
 local ev = 'wtf_redis:response'
 
 local function abl(a, ...)
     local n = select('#', ...)
-    if n <= 1 then return a end
+    if n == 0 then return nil end
+    if n == 1 then return a end
     return a, abl(...)
 end
 
 -- send to server
-function Redis(cmd, ...)
+local function redis(cmd, ...)
     local cb = select(select('#', ...), ...)
     local id = GetRandomIntInRange(2^32)
     cbs[id] = cb
@@ -26,3 +29,12 @@ AddEventHandler(ev, function(id, err, res)
         cbs[id] = nil
     end
 end)
+
+-- metatable to handle Redis.<cmd>
+setmetatable(Redis, {
+    __index = function(_, cmd)
+        return function(...)
+            return redis(cmd, ...)
+        end
+    end,
+})
