f87c03bd31c8a4b9e0a4e85b1ba34957f22dfb35
wtf <49658883+wtf-fivem-mods@users.noreply.github.com>
2019-04-17 12:14:58 -0700

    Continued work on memoization

--- README.md
+++ README.md
@@ -14,3 +14,33 @@ Redis client for FiveM
   - Redis is inherently more flexible than MySQL in offering a "shared-heap"
   - Persistence types are analogous to local data structures
   - No schemas required
+
+## Current State
+
+wtf_redis supports imperitive synchronous and / async calls (through memoization) and doesn't require a server script for your resource. Client to server communication is handled by wtf_redis, so all you need to do is retrieve and store your data.
+    
+**No callback hell or server/client events!**
+    
+There are two ways of making calls to Redis, the first is synchronous, which works great on threads where UI isn't actively displayed, or when storing any information.
+
+For threads where UI is actively displayed in a loop (e.g. `Citizen.Wait(1)` loops), wtf_redis support memoized calls to retrieve data.
+
+- Standard calls are synchronous, e.g.
+        
+      Redis.<cmd>(...) => result, error
+  
+- Memoized calls allow the same in-line syntax, the initial call is synchronous, however, subsequent calls are returned instantly and updated based on the memoization interval.
+
+    Updates are performed asynchronously so your UI never flickers / stutters.
+
+    Memoized calls are first synchronous, then receive async value updates, e.g.
+
+        -- During the first iteration of the loop the value is retrieved from Redis
+        -- thereafter the loop continues and `myval` is instantly returned.
+        -- It's value is updated asynchronously after 100 iterations, or in this
+        -- example, 100 ticks of the game timer.
+        while true do
+            Citizen.Wait(1)
+            local myval = Redis.memoize(100).get("somekey")
+            -- some ui code using myval
+        end
--- lib/redis.lua
+++ lib/redis.lua
@@ -7,63 +7,6 @@ local cached = {}
 -- local EV = string.format('wtf_redis:ns_%s', ns)
 local EV = 'wtf_redis:response'
 
-local function join(...)
-    local args = {...}
-    local s = args[1]
-    for i=2, #args do
-        s = s .. args[i]
-    end
-    return s
-end
-
-local function unpackFrom(n, t)
-    if #t == 0 then return nil end
-    if n == #t then return t[#t] end
-    local a = t[n]
-    return a, unpackAt(n + 1, t)
-end
-
-local mcoro = coroutine.create(function()
-        while true do
-            local ckey = coroutine.yield()
-            if cached[ckey] ~= nil then
-                Citizen.CreateThread(function()
-                    local args = cached[ckey].args
-                    local cmd = args[1]
-                    local res, err = Redis[cmd](unpackFrom(2, args))
-                    cached[ckey].res = res
-                    cached[ckey].err = err
-                end)
-            end
-        end
-end)
-
-local _memoize = {}
-setmetatable(_memoize, {
-    __index = {
-        get = function(...)
-            local ckey = join('get', ...)
-            if cached[ckey] == nil then
-                cached[ckey] = {
-                    i = 0,
-                    res = nil,
-                    err = nil,
-                    args = {'get', ...}
-                }
-                local res, err = Redis['get'](...)
-                cached[ckey].res = res
-                cached[ckey].err = err
-            else
-                cached[ckey].i = cached[ckey].i + 1
-                if cached[ckey].i % 100 == 0 then
-                    coroutine.resume(mcoro, ckey)
-                end
-            end
-            return cached[ckey].res, cached[ckey].err
-        end,
-    },
-})
-
 -- send to server
 local function redis(cmd, ...)
     local args = {...}
@@ -86,14 +29,59 @@ AddEventHandler(EV, function(id, err, res)
     end
 end)
 
+local _memoize -- see setmetatable below
+
 -- metatable to handle Redis.<cmd>
 setmetatable(Redis, {
     __index = function(_, cmd)
         return function(...)
-            if cmd == 'memoized' then
+            if cmd == 'memoize' then
+                _memoize.ival = select('1', ...) or 100
                 return _memoize
             end
             return redis(cmd, ...)
+        end
+    end,
+})
+
+local function join(...)
+    local args = {...}
+    local s = args[1]
+    for i=2, #args do
+        s = s .. args[i]
+    end
+    return s
+end
+
+local function unpackFrom(n, t)
+    if #t == 0 then return nil end
+    if n == #t then return t[#t] end
+    local a = t[n]
+    return a, unpackFrom(n + 1, t)
+end
+
+_memoize = {}
+setmetatable(_memoize, {
+    __index = function(_, cmd)
+        return function(...)
+            local ckey = join(cmd, ...)
+            if cached[ckey] == nil then
+                cached[ckey] = { i = 0, ival = _memoize.ival, res = nil, err = nil, args = {cmd, ...} }
+                local res, err = Redis[cmd](...)
+                cached[ckey].res = res
+                cached[ckey].err = err
+            else
+                cached[ckey].i = cached[ckey].i + 1
+                if cached[ckey].i % cached[ckey].ival == 0 then
+                    Citizen.CreateThread(function()
+                        local args = cached[ckey].args
+                        local res, err = Redis[cmd](unpackFrom(2, args))
+                        cached[ckey].res = res
+                        cached[ckey].err = err
+                    end)
+                end
+            end
+            return cached[ckey].res, cached[ckey].err
         end
     end,
 })
